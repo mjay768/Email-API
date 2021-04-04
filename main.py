6ec43ef9585752a5f11930a6d1318c2b076420da
mjay768 <MJL!ves4Ever>
2021-04-04 15:17:30 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+
+contact_details = []
+
+@app.route('/',methods=['GET'])
+def get_stored_contacts():
+    return jsonify(contact_details)
+
+@app.route('/', methods=['POST'])
+def add_contact_card():
+    request_data = request.get_json()
+    for item in request_data:
+        contact_card = {
+            "name" : item["name"]
+        }
+        contact_details.append(contact_card)
+
+
+
+
+    return jsonify({"status": "200","data": contact_details})
+
+if __name__ == '__main__':
+    app.run()
